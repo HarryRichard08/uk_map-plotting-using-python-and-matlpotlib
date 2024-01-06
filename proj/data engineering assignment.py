36a0b6c70f953a1c51079c9b695252d3e73fcbdd
HarryRichard08 <75106918+HarryRichard08@users.noreply.github.com>
2024-01-06 13:26:57 +0530

    Update data engineering assignment.py

--- proj/data engineering assignment.py
+++ proj/data engineering assignment.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # importing modules 
-
-# In[38]:
 
 
 import pandas as pd
@@ -11,9 +5,6 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 
-# # import data 
-
-# In[39]:
 
 
 data = pd.read_csv(r'GrowLocations.csv')
@@ -30,9 +21,6 @@ min_lat = -10.592
 max_lat = 1.6848
 
 
-# # Data cleaning 
-
-# In[41]:
 
 
 filtered_grow_df = grow_df[
@@ -41,54 +29,9 @@ filtered_grow_df = grow_df[
 ]
 
 
-# # loading the map to overlay the sensory locations 
-
-# In[42]:
-
-
-map_img = plt.imread(r'map7.png')
-
-
-# # data plotting 
-
-# In[43]:
-
-
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.imshow(map_pic, extent=[min_long, max_long, min_lat, max_lat])
-
-
-# # using marker green leaf to show sensor location 
-
-# In[44]:
-
-
-ax.scatter(good_data['Longitude'], good_data['Latitude'], zorder=1, alpha=0.6, 
-           c='green', s=50, marker='p', edgecolors='black', linewidth=0.5)
-
-
-# # setting the title and limits for the plotting 
-
-# In[45]:
 
 
 
-
-ax.set_title('Sensor Locations on UK Map')
-ax.set_xlim(min_long, max_long)
-ax.set_ylim(min_lat, max_lat)
-
-box = Rectangle((min_long, min_lat),
-                max_long - min_long,
-                max_lat - min_lat,
-                fill=False, color="red", linewidth=2)
-
-ax.add_patch(box)
-
-plt.show()
-
-
-# In[46]:
 
 
 # bringing every thing together 
@@ -113,21 +56,11 @@ ax.add_patch(box)
 plt.show()
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
 
 # # github link : https://github.com/HarryRichard08/uk_map-plotting-using-python-and-matlpotlib/tree/main/proj
-
-# In[ ]:
 
 
 
